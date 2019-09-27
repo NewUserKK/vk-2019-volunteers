b3fa349796d321c4e8f3a792c72d9253e7d25041
Konstantin Kostin <newuser9929@gmail.com>
2019-09-27 19:08:19 +0300

    Add fragments and setup main activity

--- Android/app/src/main/java/ru/newuserkk/volunteers/presentation/MainActivity.kt
+++ Android/app/src/main/java/ru/newuserkk/volunteers/presentation/MainActivity.kt
@@ -1,13 +1,59 @@
 package ru.newuserkk.volunteers.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.newuserkk.volunteers.R
+import ru.newuserkk.volunteers.presentation.common.NavigationResultListener
+import kotlin.properties.Delegates
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavHost {
+
+    private val navHostFragmentId: Int = R.id.nav_host_fragment
+    private lateinit var navigationController: NavController
+
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigationController = Navigation.findNavController(this, navHostFragmentId)
+        setupToolbar(main_toolbar)
+    }
+
+    private fun setupToolbar(toolbar: Toolbar) {
+        this.toolbar = toolbar
+        setSupportActionBar(toolbar)
+        val appBarConfiguration = AppBarConfiguration(navGraph = navigationController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun getNavController(): NavController {
+        return navigationController
+    }
+
+    /**
+     * https://medium.com/google-developer-experts/using-navigation-architecture-component-in-a-large-banking-app-ac84936a42c2
+     */
+    fun navigateBackWithResult(action: String, result: Bundle) {
+        val childFragmentManager =
+            supportFragmentManager.findFragmentById(navHostFragmentId)?.childFragmentManager
+        var backStackListener: FragmentManager.OnBackStackChangedListener by Delegates.notNull()
+        backStackListener = FragmentManager.OnBackStackChangedListener {
+            (childFragmentManager?.fragments?.get(0) as NavigationResultListener).onNavigationResult(
+                action,
+                result
+             )
+            childFragmentManager.removeOnBackStackChangedListener(backStackListener)
+        }
+        childFragmentManager?.addOnBackStackChangedListener(backStackListener)
+        navController.popBackStack()
     }
 }
---
+++ Android/app/src/main/java/ru/newuserkk/volunteers/presentation/person/PersonRegistrationFragment.kt
@@ -0,0 +1,23 @@
+package ru.newuserkk.volunteers.presentation.person
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.newuserkk.volunteers.R
+
+
+class PersonRegistrationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_person_registration, container, false)
+    }
+
+
+}
