b346fb9931b312561914617cbacb1a37f7e3f16c
Konstantin Kostin <newuser9929@gmail.com>
2019-09-28 01:26:13 +0300

    Add navigation to event details

--- Android/app/src/main/java/ru/newuserkk/volunteers/domain/event/model/Event.kt
+++ Android/app/src/main/java/ru/newuserkk/volunteers/domain/event/model/Event.kt
@@ -1,14 +1,15 @@
 package ru.newuserkk.volunteers.domain.event.model
 
 import ru.newuserkk.volunteers.domain.museum.model.Museum
+import java.io.Serializable
 import java.util.*
 
 data class Event(
     val name: String,
     val description: String,
     val museum: Museum,
-    val avatarUri: String,
-    val id: Long = 0,
+    val avatarUri: String? = null,
     val dateStart: Date,
-    val dateEnd: Date
-)
+    val dateEnd: Date,
+    val id: Long = 0
+    ): Serializable
