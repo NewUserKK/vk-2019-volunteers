b5daf2e5193d02bc36ff4cee3ab02c98a9ce81c8
Konstantin Kostin <newuser9929@gmail.com>
2019-09-27 18:04:41 +0300

    Add main activity

---
+++ Android/app/src/main/java/ru/newuserkk/volunteers/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.newuserkk.volunteers.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.newuserkk.volunteers.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
