7b53765920a7dd382271b5b7f53c0bdf3b2768b8
Damtev <damtev@yandex.ru>
2019-09-28 16:35:12 +0300

    Password is hidden now

--- Android/app/src/main/java/ru/wa285/volunteers/presentation/person/PersonProfileFragment.kt
+++ Android/app/src/main/java/ru/wa285/volunteers/presentation/person/PersonProfileFragment.kt
@@ -1,16 +1,11 @@
 package ru.wa285.volunteers.presentation.person
 
-import org.kodein.di.generic.instance
 import ru.wa285.volunteers.R
-import ru.wa285.volunteers.domain.event.EventRepository
-import ru.wa285.volunteers.domain.event.model.Event
-import ru.wa285.volunteers.domain.museum.MuseumRepository
 import ru.wa285.volunteers.presentation.common.AbstractFragment
-import ru.wa285.volunteers.presentation.event.EventListRecyclerViewAdapter
 
 
 class ProfileFragment : AbstractFragment() {
 
     override val layoutResId: Int = R.layout.fragment_profile
-    
+
 }
