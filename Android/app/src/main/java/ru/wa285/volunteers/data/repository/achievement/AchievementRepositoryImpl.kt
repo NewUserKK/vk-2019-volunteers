1781c2b3ac0464b74a4a0e512c4cc059675069ed
Konstantin Kostin <newuser9929@gmail.com>
2019-09-28 16:50:43 +0300

    Fix image size

--- Android/app/src/main/java/ru/wa285/volunteers/data/repository/achievement/AchievementRepositoryImpl.kt
+++ Android/app/src/main/java/ru/wa285/volunteers/data/repository/achievement/AchievementRepositoryImpl.kt
@@ -1,17 +1,8 @@
 package ru.wa285.volunteers.data.repository.achievement
 
 import retrofit2.Retrofit
-import ru.wa285.volunteers.data.common.exception.BadResponseException
 import ru.wa285.volunteers.data.net.VolunteersAchievementApiService
-import ru.wa285.volunteers.data.net.VolunteersMuseumApiService
-import ru.wa285.volunteers.data.net.toOperationResult
-import ru.wa285.volunteers.data.net.tryConnect
-import ru.wa285.volunteers.domain.common.OperationResult
-import ru.wa285.volunteers.domain.museum.MuseumRepository
-import ru.wa285.volunteers.domain.museum.model.Museum
 import ru.wa285.volunteers.domain.person.AchievementRepository
-import ru.wa285.volunteers.domain.person.model.Achievement
-import java.io.IOException
 
 class AchievementRepositoryImpl(private val retrofit: Retrofit) : AchievementRepository {
 
