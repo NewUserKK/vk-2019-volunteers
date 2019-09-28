8fc94765df10d3692e9bc0dc92bc625ec16d9455
Konstantin Kostin <newuser9929@gmail.com>
2019-09-28 13:50:03 +0300

    Adjust auth layout

--- Android/app/src/main/java/ru/wa285/volunteers/presentation/museum/MuseumListRecyclerViewAdapter.kt
+++ Android/app/src/main/java/ru/wa285/volunteers/presentation/museum/MuseumListRecyclerViewAdapter.kt
@@ -1,6 +1,5 @@
 package ru.wa285.volunteers.presentation.museum
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +9,17 @@ import kotlinx.android.synthetic.main.item_list_museum.view.*
 import ru.wa285.volunteers.R
 import ru.wa285.volunteers.domain.museum.model.Museum
 
-class MuseumListRecyclerViewAdapter(private val values: List<Museum>) : RecyclerView.Adapter<MuseumListRecyclerViewAdapter.ViewHolder>() {
+class MuseumListRecyclerViewAdapter(private val values: List<Museum>) :
+    RecyclerView.Adapter<MuseumListRecyclerViewAdapter.ViewHolder>() {
 
     var onClickListener: ((Museum) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list_museum, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_list_museum,
+            parent,
+            false
+        )
         return ViewHolder(view)
     }
 
@@ -23,7 +27,6 @@ class MuseumListRecyclerViewAdapter(private val values: List<Museum>) : Recycler
         return values.size
     }
 
-    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
         holder.apply {
@@ -35,7 +38,7 @@ class MuseumListRecyclerViewAdapter(private val values: List<Museum>) : Recycler
         }
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val nameView: TextView = view.item_list_museum_name
         val placeView: TextView = view.item_list_museum_place
     }
