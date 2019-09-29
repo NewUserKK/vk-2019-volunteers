cef8c7183b671f675b7fec42032b797744a077a2
Alexey Menshutin <menshutin@yandex-team.ru>
2019-09-29 05:06:46 +0300

    dslfkmsd

--- Backend/volunteer/src/main/java/ru/ifmo/volunteer/controller/UserController.java
+++ Backend/volunteer/src/main/java/ru/ifmo/volunteer/controller/UserController.java
@@ -74,7 +74,7 @@ public class UserController {
   @ApiOperation(
       value = "Добавляет пароль к юзеру",
       produces = "application/json")
-  @PutMapping("/register")
+  @PostMapping("/register")
   public void register(@RequestParam String login, @RequestParam String password) {
     System.err.println(password);
     userService.register(login, password);
